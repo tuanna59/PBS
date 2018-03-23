b98845994de53742e56c81d9dd98a6ce64c086a0
Tuan Nguyen <nguyenanhtuan090596@gmail.com>
2018-03-23 07:44:11 +0700

    Init Project

---
+++ app/src/main/java/team15/capstone2/pbs/MainActivity.java
@@ -0,0 +1,13 @@
+package team15.capstone2.pbs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
